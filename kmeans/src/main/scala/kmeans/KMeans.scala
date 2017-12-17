9493715b07836779b7baad648dd00dcb92994488
Pulkit Arora <pulkitarora26@gmail.com>
2017-12-18 01:17:24 +0530

    week 3 assignment complete

--- kmeans/src/main/scala/kmeans/KMeans.scala
+++ kmeans/src/main/scala/kmeans/KMeans.scala
@@ -43,7 +43,8 @@ class KMeans {
   }
 
   def classify(points: GenSeq[Point], means: GenSeq[Point]): GenMap[Point, GenSeq[Point]] = {
-    ???
+    val map = points.groupBy(findClosest(_, means))
+    means.groupBy(x => x).map { case (point, _) => (point, map.getOrElse(point, GenSeq())) }
   }
 
   def findAverage(oldMean: Point, points: GenSeq[Point]): Point = if (points.length == 0) oldMean else {
@@ -59,23 +60,25 @@ class KMeans {
   }
 
   def update(classified: GenMap[Point, GenSeq[Point]], oldMeans: GenSeq[Point]): GenSeq[Point] = {
-    ???
+    oldMeans.map(old => findAverage(old, classified(old)))
   }
 
   def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
-    ???
+    oldMeans zip newMeans forall { case (o, n) => o.squareDistance(n) <= eta }
   }
 
   @tailrec
   final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
-    if (???) kMeans(???, ???, ???) else ??? // your implementation need to be tail recursive
+    val classified = classify(points, means)
+    val newMeans = update(classified, means)
+    if (!converged(eta)(means, newMeans)) kMeans(points, newMeans, eta) else newMeans
   }
 }
 
 /** Describes one point in three-dimensional space.
- *
- *  Note: deliberately uses reference equality.
- */
+  *
+  *  Note: deliberately uses reference equality.
+  */
 class Point(val x: Double, val y: Double, val z: Double) {
   private def square(v: Double): Double = v * v
   def squareDistance(that: Point): Double = {
