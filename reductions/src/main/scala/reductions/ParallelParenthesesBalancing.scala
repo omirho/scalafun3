d6a903de49837da9b3f13c3984892f9dfbbd952f
Pulkit Arora <pulkitarora26@gmail.com>
2017-12-17 16:29:41 +0530

    parallel balancing complete

--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -41,22 +41,41 @@ object ParallelParenthesesBalancing {
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def balanceHelper(index: Int, openCount: Int): Boolean = {
+      if (index == chars.length) openCount == 0
+      else if (chars(index) == '(') balanceHelper(index + 1, openCount + 1)
+      else if (chars(index) == ')') (openCount > 0) && balanceHelper(index + 1, openCount - 1)
+      else balanceHelper(index + 1, openCount)
+    }
+    balanceHelper(0, 0)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    @tailrec
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      if (idx >= until) (arg1, arg2)
+      else {
+        var (open, close) = (arg1, arg2)
+        if (chars(idx) == '(') open += 1
+        else if (chars(idx) == ')') if (open > 0) open -= 1 else close += 1
+        traverse(idx + 1, until, open, close)
+      }
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = from + (until - from) / 2
+        val ((l1, l2), (r1, r2)) = parallel(reduce(from, mid), reduce(mid, until))
+        val a = 0 max (l1 - r2)
+        (l1 + r1 - a, l2 + r2 - a)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
